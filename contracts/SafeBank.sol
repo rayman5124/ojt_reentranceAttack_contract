27d45ded9713143d23dce98b2f8bba272b9306c7
Rayman.no <rayman.no@crypted.co.kr>
2023-05-09 15:28:58 +0900

    update: trim logs

--- contracts/SafeBank.sol
+++ contracts/SafeBank.sol
@@ -17,8 +17,6 @@ contract SafeBank {
         uint256 currentBalance = balanceOf[msg.sender];
         console.log("current balance of %s : %s ", msg.sender, currentBalance);
         require(currentBalance != 0, "not enough balance to withdraw"); // check 여기 메세지는 출력아 안된다.
-        // msg.sender.call 을 실행하면서 내부적인 callback으로 다시 withdraw를 돌게되는데 그때 위의 require에서 실패한다.
-        // 최종적으로는 첫 msg.sender.call 자체가 실패한게 되므로 제일 밑의 require(success)의 메세지가 출력되게 된다.
         balanceOf[msg.sender] = 0; // effects to the state variables
         (bool success, ) = msg.sender.call{value: currentBalance}("");
         require(success, "withdrwal failed");
